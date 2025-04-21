51eca97fbc996a542db185cbe81ca09112ddedd3
Derion Williams <derionwllms@gmail.com>
2025-04-21 16:51:05 -0500

    added a field for crew notes

--- react-src/src/schemas/contactForm.schema.ts
+++ react-src/src/schemas/contactForm.schema.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 
 export const contactFormSchema = z.object({
-    contactInformation: z.object({
-        "full-name": z.string().min(2).max(50),
-        email: z.string().min(2).max(50),
-        "phone-number": z.string().min(2).max(50),
-    }),
-    "moving-from": z.string().min(2).max(50),
-    "moving-to": z.string().min(2).max(50),
-    consent: z.boolean(),
+  contactInformation: z.object({
+    "full-name": z.string().min(2).max(50),
+    email: z.string().min(2).max(50),
+    "phone-number": z.string().min(2).max(50),
+  }),
+  "moving-from": z.string().min(2).max(50),
+  "moving-to": z.string().min(2).max(50),
+  consent: z.boolean(),
+  "crew-notes": z.string().max(2200).optional(),
 });
 
 export type ContactForm = z.infer<typeof contactFormSchema>;
